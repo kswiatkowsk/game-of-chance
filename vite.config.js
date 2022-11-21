e56d39438f20b326a6937ec2fb2a7cde5e92e168
kswiatkowsk <kswiatkowsk@gmail.com>
2022-11-21 07:08:59 +0100

    rollup aliases & ts paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import alias from "@rollup/plugin-alias";
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
+  plugins: [
+    wasm(),
+    topLevelAwait(),
+    alias({
+      entries: [
+        { find: "@types", replacement: "./src/types" },
+        { find: "@objects", replacement: "./src/objects" },
+      ],
+    }),
+  ],
 });
